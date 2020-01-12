821373f7404cb9ec0c3a9028a988a7211cd1bd94
Diego Collares <diegocollares@brq.com>
2020-01-11 22:22:55 -0300

    install and config packeg to jwt

---
+++ Settings.cs
@@ -0,0 +1,7 @@
+namespace Shop 
+{
+  public static class Settings
+  {
+    public static string Secret = "$2y$12$rzeUCcYFeegU91ajbZw1FOrsQcULZNj2d.0py8ur.3ii.AMF5Faaa"
+  }
+}
--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Shop.Data;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Shop
 {
@@ -22,6 +25,23 @@ namespace Shop
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            var key = Encoding.ASCII.GetBytes(Settings.Secret);
+
+            services.AddAuthentication(x => {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer( x => {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
             // services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
 
             services.AddDbContext<DataContext>(options => {
@@ -42,6 +62,8 @@ namespace Shop
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
